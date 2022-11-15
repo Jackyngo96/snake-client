2815bd42d96266ccff539a9856cbff8245160a9c
Jacky Ngo <jacky.ngo96@gmail.com>
2022-11-15 10:05:33 -0500

    Export Connect Funtion

--- play.js
+++ play.js
@@ -1,24 +1,3 @@
-const net = require("net");
+const net = require("net"); 
+const connect = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  
-  conn.on("connect", () => {
-    conn.write("Hello from client!");
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
