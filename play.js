df76a9e6c6f1fe46479d4984967ecd5bb366298e
Jacky Ngo <jacky.ngo96@gmail.com>
2022-11-16 13:45:12 -0500

    Create handle UserInput function

--- play.js
+++ play.js
@@ -2,4 +2,24 @@ const connect = require("./client");
 
 // establishes a connection with the game server
 console.log("Connecting ...");
-  connect();
+ const conn = connect();
+
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    // stdin.setEncoding("utf8");
+    stdin.resume();
+    
+    const handleUserInput = function () {
+      stdin.on('data', (messageFromStdIn) => {
+        conn.write('you typed:', messageFromStdIn);
+        if (messageFromStdIn === '\u0003') {
+          process.exit();
+        }
+      });
+    };
+    stdin.on("data", handleUserInput);
+    return stdin;
+  }; 
+
+  setupInput()
