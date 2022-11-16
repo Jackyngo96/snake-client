0d2e7558fc008d9df1daa87d90a19175b6c3a307
Jacky Ngo <jacky.ngo96@gmail.com>
2022-11-16 16:27:40 -0500

    Add messaging and movement

--- client.js
+++ client.js
@@ -10,31 +10,16 @@ const connect = function () {
     conn.on("data", (data) => {
       console.log("Server says: ", data); 
     });
-    //upon connection to the server, prints out "Successfully connected to game server"  
+//upon connection to the server, prints out "Successfully connected to game server"  
     conn.on("connect", () => {
       console.log("Successfully connected to game server");
-      //sends initials of client to server upon connecting
+//sends initials of client to server upon connecting
       conn.write("Name: JN")
     });
-    
-    // conn.on("connect", () => {
-    //   conn.write("Move: up")
-    //   conn.write("Move: left") 
-    //   conn.write("Move: down") 
-    //   setInterval(() => {
-    //     conn.write("Move: up");
-    //   }, 1000);
-    //   setTimeout(() => {
-    //     conn.write("Move: down");
-    //   }, 5000);
-    //   setTimeout(() => {
-    //     conn.write("Move: right");
-    //   }, 9000);
-    // });
-    // interpret incoming data as text
     conn.setEncoding("utf8");
   
     return conn;
   };
+  
   module.exports = {connect}
   
--- input.js
+++ input.js
@@ -1,19 +1,49 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
-  // stdin.setEncoding("utf8");
+  stdin.setEncoding("utf8");
   stdin.resume();
-  
-  const handleUserInput = function () {
-    stdin.on('data', (messageFromStdIn) => {
-      //conn.write('you typed:', messageFromStdIn);
-      if (messageFromStdIn === '\u0003') {
-        process.exit();
-      }
-    });
-  };
   stdin.on("data", handleUserInput);
-  return stdin;
-};  
+};
+  
 
-module.exports = {setupInput}
+
+  const handleUserInput = function (key) {
+  // move up if w is pressed
+  if (key === "w") {
+    // setInterval(() => {
+    connection.write("Move: up");
+    // }, 1000);
+    // console.log("Move: up");
+    }
+  if (key === "a") {
+    //setInterval(() => {
+    connection.write("Move: left");
+    // }, 1000);
+    // console.log("Move: left");
+  } 
+  if (key === "s") {
+    //setInterval(() => {
+    connection.write("Move: down");
+    //}, 1000);
+    // console.log("Move: down");
+  } 
+  if (key === "d") {
+    //setInterval(() => {
+    connection.write("Move: right");
+    // }, 1000);
+    // console.log("Move: right");
+  }
+  if (key === "/") { 
+    connection.write("Say: HELLO")
+  }
+  // exit program if Ctrl C is pressed
+  if (key === "\u0003") {
+      process.exit();
+  }
+};
+
+module.exports = { setupInput };
